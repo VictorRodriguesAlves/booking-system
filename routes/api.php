5fa07f80a76bf0fcd369b658957a5c97297f3f24
Victor Alves <longovictor23@gmail.com>
2025-04-16 08:04:31 -0300

    Remove ping route

--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,7 @@
 <?php
 
 use App\Http\Controllers\ReservationController;
-use App\Models\User;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/ping', function (Request $request) {
-    return response()->json([
-        'pong' => true,
-        'users' => User::all()
-
-    ]);
-});
 
 Route::prefix('reservation')->group( function () {
     Route::post('/', [ReservationController::class, 'store']);
